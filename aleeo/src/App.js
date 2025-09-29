60054bf150b444fc789eb7e0809bd348d50b0188
HunJer93 <jhunton22@gmail.com>
2025-09-29 16:05:01 -0400

    added axios and added base api proof of concept

--- aleeo/src/App.js
+++ aleeo/src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
+import Users from './components/Users';
+import { useEffect, useState } from 'react';
+
+// move to store 
+const API_URL = "http://localhost:3000/api/v1/users"
+
+function getAPIData() {
+  return axios.get(API_URL).then((response) => response.data)
+}
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    let mounted = true;
+    getAPIData().then((items) => {
+      if (mounted) {
+        setUsers(items);
+      }
+    });
+
+    return () => (mounted = false);
+
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello</h1>
+      <Users users={users}/>
     </div>
   );
 }
