b8dff6435736e7f0ee3eb3aeab3076bed938d62d
HunJer93 <jhunton22@gmail.com>
2025-10-02 14:42:09 -0400

    moved form into separate block

--- aleeo/src/components/UserLogin.jsx
+++ aleeo/src/components/UserLogin.jsx
@@ -13,42 +13,46 @@ function UserLogin(props) {
     console.log("Sign-in password: ", password);
   };
 
+  // form for signing in
+  const signinForm = () => 
+      <Stack gap="8" maxW="sm" css={{ "--field-label-width": "96px" }}>
+        <Field.Root orientation={"horizontal"} required>
+          <Field.Label>
+            Username
+            <Field.RequiredIndicator />
+          </Field.Label>
+          <Input 
+            value={username}
+            placeholder='Username' 
+            flex="1"
+            onChange={(e) => setUsername(e.target.value)}  
+            />
+        </Field.Root>
 
-  return <div>
-    <Stack gap="8" maxW="sm" css={{ "--field-label-width": "96px" }}>
-      <h1>Marshall Flinkman</h1>
-      <Field.Root orientation={"horizontal"} required>
-        <Field.Label>
-          Username
-          <Field.RequiredIndicator />
-        </Field.Label>
-        <Input 
-          value={username}
-          placeholder='Username' 
-          flex="1"
-          onChange={(e) => setUsername(e.target.value)}  
+        <Field.Root orientation={"horizontal"} required>
+          <Field.Label>
+            Password
+            <Field.RequiredIndicator />
+          </Field.Label>
+          <Input
+            value={password}
+            placeholder='Password' 
+            flex="1"
+            onChange={(e) => setPassword(e.target.value)}
           />
-      </Field.Root>
+        </Field.Root>
 
-      <Field.Root orientation={"horizontal"} required>
-        <Field.Label>
-          Password
-          <Field.RequiredIndicator />
-        </Field.Label>
-        <Input
-          value={password}
-          placeholder='Password' 
-          flex="1"
-          onChange={(e) => setPassword(e.target.value)}
-        />
-      </Field.Root>
+        <Button colorPalette={"blue"} variant="surface" onClick={handleSignin}>
+          Sign in
+        </Button>
+      </Stack>
 
-      <Button colorPalette={"blue"} variant="surface" onClick={handleSignin}>
-        Sign in
-      </Button>
-
-    </Stack>
-  </div>
+  return (
+    <div>
+        <h1>Marshall Flinkman</h1>
+        {signinForm()}
+    </div>
+  )
   
 }
 
