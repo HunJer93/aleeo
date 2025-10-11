e1cfbd389cea83c32d33ca9cae6ef9c329b02d99
HunJer93 <jhunton22@gmail.com>
2025-10-11 10:26:48 -0400

    removed unsued endpoint

--- aleeo/src/utility/apiUtils.js
+++ aleeo/src/utility/apiUtils.js
@@ -1,14 +1,5 @@
 import apiClient from "./api"
 
-export const getUsers = async () => {
-  try {
-    const response = await apiClient.get('/users');
-    return response.data;
-  } catch (error) {
-      console.error('Error fetching data:', error);
-  }
-};
-
 export const userLogin = async (params) => {
   try {
     const response = await apiClient.post('/login', params)
