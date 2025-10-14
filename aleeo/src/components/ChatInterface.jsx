55d52a99419dfa477f014f348d4cb10ae637d209
HunJer93 <jhunton22@gmail.com>
2025-10-14 09:46:14 -0400

    fixed message scroll window and added scrolling gradient

--- aleeo/src/components/ChatInterface.jsx
+++ aleeo/src/components/ChatInterface.jsx
@@ -113,8 +113,22 @@ function ChatInterface(props) {
         <Container height="100%">
           <Heading size="lg" paddingTop={'1rem'} paddingBottom={'2rem'}>Current Chat ({currentChat?.title})</Heading>
           {/* scroll area for messages */}
-          <ScrollArea.Root height={"80%"}>
-            <ScrollArea.Viewport>
+          <ScrollArea.Root height={"55rem"}>
+            <ScrollArea.Viewport
+              css={{
+                "--scroll-shadow-size": "4rem",
+                maskImage:
+                  "linear-gradient(#000,#000,transparent 0,#000 var(--scroll-shadow-size),#000 calc(100% - var(--scroll-shadow-size)),transparent)",
+                "&[data-at-top]": {
+                  maskImage:
+                    "linear-gradient(180deg,#000 calc(100% - var(--scroll-shadow-size)),transparent)",
+                },
+                "&[data-at-bottom]": {
+                  maskImage:
+                    "linear-gradient(0deg,#000 calc(100% - var(--scroll-shadow-size)),transparent)",
+                },
+              }}
+            >
             <ScrollArea.Content>
               {messageBuilder(currentChat?.messages)}
             </ScrollArea.Content>
